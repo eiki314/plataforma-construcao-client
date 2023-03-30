e4db57ea268c8ce1429b7e0c6822b72263c2b672
schelip <fcominscheffel@gmail.com>
2023-03-29 21:20:03 -0300

    T002.4: add game route buttons to nav bar and home page

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,20 +2,37 @@
   <v-app dark>
     <v-navigation-drawer :mini-variant="miniVariant" app>
       <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
+        <v-list-item router exact to="/">
           <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
+            <v-icon>mdi-home</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
+            <v-list-item-title>Home</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
+        <v-divider inset></v-divider>
+        <v-list>
+          <v-list-item
+            v-for="game in games"
+            :key="game.id"
+            router
+            exact
+            :style="`background: ${game.colorHex}`"
+          >
+            <v-list-item-action>
+              <v-img
+                v-if="game.icon"
+                :src="`data:image/jpg;base64,${game.icon}`"
+                max-width="25"
+              />
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title
+                ><b>{{ game.name }}</b></v-list-item-title
+              >
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
       </v-list>
     </v-navigation-drawer>
     <v-app-bar fixed app>
@@ -38,6 +55,10 @@
     </v-navigation-drawer>
     <v-footer app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
+      <v-spacer />
+      <span
+        >Icons by <a target="_blank" href="https://icons8.com">Icons8</a></span
+      >
     </v-footer>
   </v-app>
 </template>
@@ -63,6 +84,22 @@ export default {
       rightDrawer: false,
       title: "Plataforma Construção",
     };
+  },
+  async fetch() {
+    if (!this.$store.state.game.games) {
+      await this.$store.dispatch("game/fetchGames");
+    }
+  },
+  computed: {
+    loggedIn() {
+      return this.$auth.loggedIn;
+    },
+    user() {
+      return this.$auth.user;
+    },
+    games() {
+      return this.$store.state.game.games;
+    },
   },
 };
 </script>
